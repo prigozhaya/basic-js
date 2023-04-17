35ce481782eb0e5cbf1e12d06e91ea0a792c69c0
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-04-17 23:31:35 +0300

    feat: add solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  
+  let strSeparator = typeof options["separator"] !== "undefined" ? options.separator : "+";
+  let repeatCount = typeof options["repeatTimes"] !== "undefined" ? options.repeatTimes : 1;
+  let addStrRepeatCount = typeof options["additionRepeatTimes"] !== "undefined" ? options.additionRepeatTimes : typeof options["additionRepeatTimes"] === "undefined" && typeof options["addition"] !== "undefined"?1:0;
+  let addStr = typeof options["addition"] !== "undefined" ? options.addition : "";
+  let addStrSeparator = typeof options["additionSeparator"] !== "undefined" ? options.additionSeparator : "|";
+  
+  let addStrResult = addStr
+  for (let i = 1; i < addStrRepeatCount; i++) {
+    addStrResult = addStrResult + addStrSeparator + addStr;
+  }
+
+  let result = str+addStrResult;
+  for (let i = 1; i < repeatCount; i++) {
+    result = result + strSeparator + str+addStrResult;
+  }
+  return result;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(!date){
+    return 'Unable to determine the time of year!'
+  }
+  dateVariable = new Date();
+
+  if(!isNaN(Date.parse(new Date(dateVariable))) && dateVariable !== 'Invalid Date'&&date instanceof Date && date && Object.prototype.toString.call(date) === "[object Date]"&& !isNaN(date)){
+  let season = date.getMonth()<2? 'winter': date.getMonth()<5?'spring': date.getMonth()<8? 'summer': date.getMonth()<11?'autumn': 'winter'
+  return season
+  }else if(dateVariable<0){
+    throw new Error('Invalid date!');
+  }else{
+    throw new Error('Invalid date!');
+
+  }
+  
 }
 
 module.exports = {
